464589018b337c861d4442b530b909f3a506aec5
jmoore <josh@glencoesoftware.com>
2011-12-10 16:06:48 +0100

    manual config

--- src/test/java/SpringTest.java
+++ src/test/java/SpringTest.java
@@ -1,10 +1,11 @@
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.access.ContextSingletonBeanFactoryLocator;
-
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringTest {
 
@@ -13,15 +14,27 @@ public class SpringTest {
      * on the classpath.
      */
     @Test
+    @SuppressWarnings("unchecked")
     public void testBeanRefContext() throws Exception {
         ApplicationContext ctx = (ApplicationContext) ContextSingletonBeanFactoryLocator
                 .getInstance().useBeanFactory("example1").getFactory();
-        
+
         // Found by name; can be multiple
         Map<String, String> someMap = ctx.getBean("someMap", Map.class);
         // Can only be one
         List<Object> someList = ctx.getBean(List.class);
+
+        assertEquals(0, someMap.size());
+        assertEquals(1, someList.size());
+        assertEquals("hi", someList.get(0));
     }
 
-    
+    @Test
+    public void testManual() throws Exception {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "classpath:integer1.xml");
+        Integer i = ctx.getBean("myint", Integer.class);
+        assertEquals(Integer.valueOf(1), i);
+    }
+
 }
